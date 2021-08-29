a915011facb82c7fbb2bfc48553b8b82927a89d7
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-08-29 09:22:08 +0100

    command line solution

--- src/main/java/training/metofficeweather/Main.java
+++ src/main/java/training/metofficeweather/Main.java
@@ -1,7 +1,29 @@
 package training.metofficeweather;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import training.metofficeweather.data.Location;
+import training.metofficeweather.data.Root;
+
 public class Main {
-    public static void main(String args[]) {
-        // Your code here!
+    static final String siteListUrl =  "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=7581460d-7f90-4bd0-bacb-e72e36e3b557";
+    static final Logger theLogger = LogManager.getLogger("Weather");
+    public static void main(String[] args) {
+        theLogger.info("Application Started");
+
+
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+        Object got = client.target(siteListUrl)
+                .request(MediaType.APPLICATION_JSON)
+                .get(Root.class);
+
+
+        theLogger.info("Got {}", got);
+
     }
 }	
